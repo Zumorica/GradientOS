7bdb13939cd5fabb55676cab71da64eb5db1cdb2
Vera Aguilera Puerto <gradientvera@outlook.com>
2023-05-01 00:00:18 +0200

    Cleanup sound file

--- hardware/sound.nix
+++ hardware/sound.nix
@@ -1,11 +1,7 @@
-{ lib, pkgs, ... }:
+{ lib, ... }:
 
-let
-  jack-libs = pkgs.runCommand "jack-libs" {} ''
-      mkdir -p "$out/lib"
-      ln -s "${pkgs.pipewire.jack}/lib" "$out/lib/pipewire"
-    '';
-in {
+{
+
   hardware.pulseaudio.enable = lib.mkForce false;
   
   security.rtkit.enable = true;
@@ -20,8 +16,5 @@ in {
   };
 
   sound.enable = true;
-  
-  environment.systemPackages = [
-    jack-libs
-  ];
+
 }
