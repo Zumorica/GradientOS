59001d7ba889fa915b258ba46ae66fdd1d158af8
Vera Aguilera Puerto <gradientvera@outlook.com>
2023-05-03 02:59:21 +0200

    Fix tiny locale mistake.

--- hosts/neith-deck/locale.nix
+++ hosts/neith-deck/locale.nix
@@ -3,7 +3,7 @@
 {
 
   # Set your time zone.
-  time.timeZone = "inAmerica/Los_Angeles";
+  time.timeZone = "America/Los_Angeles";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
