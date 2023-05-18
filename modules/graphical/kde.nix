f16c4d972132c61a4f13c7dd1ae62e216aca4df1
Vera Aguilera Puerto <gradientvera@outlook.com>
2023-05-18 17:21:51 +0200

    Use QT scaling

--- modules/graphical/kde.nix
+++ modules/graphical/kde.nix
@@ -5,6 +5,7 @@
   # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.desktopManager.plasma5.useQtScaling = true;
 
   programs.kdeconnect.enable = true;
 
