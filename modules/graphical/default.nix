98bcd8cba4a837e569965bb9f1ffc14547c668e5
Vera Aguilera Puerto <gradientvera@outlook.com>
2023-05-18 17:08:35 +0200

    Enable XWayland

--- modules/graphical/default.nix
+++ modules/graphical/default.nix
@@ -13,6 +13,9 @@
   # Enable touchpad support (enabled default in most desktopManager).
   services.xserver.libinput.enable = true;
 
+  # Enable XWayland for X11 compat when using wayland.
+  programs.xwayland.enable = true;
+
   hardware.opengl = {
     enable = true;
     driSupport = true;
