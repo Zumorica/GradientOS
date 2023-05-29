8461f59ff1aa23c0f98ffdf68aa3a6f7bb3fda96
Vera Aguilera Puerto <gradientvera@outlook.com>
2023-05-29 12:43:33 +0200

    Cleanup user ssh pub keys as well

--- users/neith/ssh.nix
+++ users/neith/ssh.nix
@@ -1,12 +1,10 @@
 { ... }:
-
-{
+let
+  ssh-pub-keys = import ../../misc/ssh-pub-keys.nix;
+in {
 
   programs.ssh.enable = true;
 
-  home.file.".ssh/id_ed25519.pub".text = 
-''
-ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKvWBwM05NhjDy+TaXqwEXJws4wt3jmuoVWkdsmCSf75 neith
-'';
+  home.file.".ssh/id_ed25519.pub".text = ssh-pub-keys.neith;
 
 }
--- users/vera/ssh.nix
+++ users/vera/ssh.nix
@@ -1,12 +1,10 @@
 { ... }:
-
-{
+let
+  ssh-pub-keys = import ../../misc/ssh-pub-keys.nix;
+in {
 
   programs.ssh.enable = true;
 
-  home.file.".ssh/id_ed25519.pub".text = 
-''
-ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOH4ZOMQX/C9x2s4D7mvP7ip1ll+Nhar+tCJiTpy1DuY vera@miracle-crusher
-'';
+  home.file.".ssh/id_ed25519.pub".text = ssh-pub-keys.vera;
 
 }
