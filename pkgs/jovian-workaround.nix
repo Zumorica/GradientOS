1440018804bc3923f9cb30db6a3f2bcf39f2d0ff
Vera Aguilera Puerto <gradientvera@outlook.com>
2023-06-15 18:35:11 +0200

    Drop Steam Deck speakers workaround.

--- pkgs/jovian-workaround.nix
+++ pkgs/jovian-workaround.nix
@@ -1,13 +1,4 @@
 (final: super: {
   # Workaround for missing kernel modules on build.
   makeModulesClosure = x: super.makeModulesClosure (x // { allowMissing = true; });
-
-  # Workaround for speakers not working with alsa-ucm-conf version 1.2.9
-  alsa-ucm-conf = super.alsa-ucm-conf.overrideAttrs (old: rec {
-    version = "1.2.8";
-    src = super.fetchurl {
-      url = "mirror://alsa/lib/${old.pname}-${version}.tar.bz2";
-      hash = "sha256-/uSnN4MP0l+WnYPaRqKyMb6whu/ZZvzAfSJeeCMmCug=";
-    };
-  });
 })
