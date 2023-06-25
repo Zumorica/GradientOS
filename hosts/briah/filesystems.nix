3e5ff0108f6fb55121b7bd070654d20bc18e9987
Vera Aguilera Puerto <gradientvera@outlook.com>
2023-06-25 13:29:30 +0200

    Add data disk to briah

--- hosts/briah/filesystems.nix
+++ hosts/briah/filesystems.nix
@@ -11,4 +11,10 @@
     fsType = "ext4";
   };
 
+  fileSystems."/data" = {
+    device = "/dev/disk/by-uuid/bf3b5f82-b859-4b22-9192-918e01ea7af1";
+    fsType = "ext4";
+    options = [ "rw" "auto" "noatime" "nofail" ];
+  };
+
 }
