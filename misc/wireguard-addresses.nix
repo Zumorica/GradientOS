ac6cdb333f0d7beea6f5249497d096c18bd06eea
Vera Aguilera Puerto <gradientvera@outlook.com>
2023-06-25 13:31:28 +0200

    Add file with wireguard addresses and public keys

---
+++ misc/wireguard-addresses.nix
@@ -0,0 +1,26 @@
+{
+  gradientnet = {
+    gradientnet = "192.168.24.0";
+    briah = "192.168.24.1";
+    miracle-crusher = "192.168.24.2";
+    vera-deck = "192.168.24.3";
+    vera-phone = "192.168.24.4";
+  };
+  
+  lilynet = {
+    lilynet = "192.168.109.0";
+    briah = "192.168.109.1";
+    miracle-crusher = "192.168.109.2";
+    neith-deck = "192.168.109.3";
+    vera-deck = "192.168.109.5";
+  };
+
+  slugcatnet = {
+    slugcatnet = "192.168.4.0";
+    briah = "192.168.4.1";
+    remie = "192.168.4.2";
+    miracle-crusher = "192.168.4.3";
+    luna = "192.168.4.4";
+    neith-deck = "192.168.4.5";
+  };
+}
---
+++ misc/wireguard-pub-keys.nix
@@ -0,0 +1,14 @@
+{
+
+  miracle-crusher = "pgPX/8I6p/6z19w4mP38VVNK9RQDXHI3rUdy9Zzv0gg=";
+  vera-deck = "3iA3etOZC+5J3zKjcFvQVw5bhiKKC0XGUQap43yMB1o=";
+  neith-deck = "p4dmhuDzBmzHvudnT2nnSTrGQY047ubuCEjEk2U2zFw=";
+  asiyah = "TODO";
+  briah = "oIa6pYWG0rIZ0lYiLlOCiR74FSoXkQOfLHssz3iB/Rc=";
+  atziluth = "TODO";
+  vera-phone = "yySbF5AY5dmoTp5v/e6wwhl9VZqTvhXufGAnUqbfZkg=";
+
+  remie = "KwDSonsjWvqwsnhA3RBMz7F2bJr30o26DwsSEOWPoig=";
+  luna = "vmBKfmh0hnqfnU8LLXpHf0Tx71QLIgiHfT7TfCVdzmU=";
+
+}
