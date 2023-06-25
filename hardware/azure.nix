364e0576573c13a4626cd6fd7316e016081daa95
Vera Aguilera Puerto <gradientvera@outlook.com>
2023-06-25 13:25:10 +0200

    Azure hardware imports networkd module.

--- hardware/azure.nix
+++ hardware/azure.nix
@@ -3,9 +3,10 @@
 
   imports = [
     "${modulesPath}/virtualisation/azure-common.nix"
+    ../modules/networkd.nix
   ];
 
+  # Forcibly disable NetworkManager.
   networking.networkmanager.enable = lib.mkForce false;
-  systemd.network.enable = true;
-  
+
 }
