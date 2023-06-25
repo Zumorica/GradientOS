9b39a3438d63617c075d8ca19dc6aa2f9824a25e
Vera Aguilera Puerto <gradientvera@outlook.com>
2023-06-25 13:31:55 +0200

    Add wireguard config to briah

--- hosts/briah/wireguard.nix
+++ hosts/briah/wireguard.nix
@@ -1,16 +1,107 @@
-{ config, pkgs, ... }:
-{
+{ config, pkgs, lib, ... }:
+
+let
+  ips = import ../../misc/wireguard-addresses.nix;
+  keys = import ../../misc/wireguard-pub-keys.nix;
+  private-key = config.sops.secrets.wireguard-private-key.path;
+  iptablesCmd = "${pkgs.iptables}/bin/iptables";
+  ip6tablesCmd = "${pkgs.iptables}/bin/ip6tables";
+  gen-post-setup = vpn: interface: 
+  "
+    ${iptablesCmd} -A FORWARD -i ${vpn} -j ACCEPT;
+    ${iptablesCmd} -t nat -A POSTROUTING -o ${interface} -j MASQUERADE;
+    ${ip6tablesCmd} -A FORWARD -i ${vpn} -j ACCEPT;
+    ${ip6tablesCmd} -t nat -A POSTROUTING -o ${interface} -j MASQUERADE
+  ";
+  gen-post-shutdown = vpn: interface:
+  "
+    ${iptablesCmd} -D FORWARD -i ${vpn} -j ACCEPT;
+    ${iptablesCmd} -t nat -D POSTROUTING -o ${interface} -j MASQUERADE;
+    ${ip6tablesCmd} -D FORWARD -i ${vpn} -j ACCEPT;
+    ${ip6tablesCmd} -t nat -D POSTROUTING -o ${interface} -j MASQUERADE
+  ";
+in {
   networking.wireguard.enable = true;
   environment.systemPackages = [ pkgs.wireguard-tools ];
 
-  networking.wireguard.interfaces = {
-
+  # Enables routing.
+  boot.kernel.sysctl = {
+    "net.ipv4.conf.all.forwarding" = lib.mkOverride 98 true;
+    "net.ipv4.conf.default.forwarding" = lib.mkOverride 98 true;
   };
 
-  networking.firewall.trustedInterfaces = [ ];
-  systemd.network.wait-online.ignoredInterfaces = [ ];
+  networking.wireguard.interfaces = {
+    gradientnet = with ips.gradientnet; {
+      ips = [ "${briah}/24" ];
+      listenPort = 1194;
+      privateKeyFile = private-key;
+      peers = [
+        {
+          allowedIPs = [ "${miracle-crusher}/32" ];
+          publicKey = keys.miracle-crusher;
+        }
+        {
+          allowedIPs = [ "${vera-deck}/32" ];
+          publicKey = keys.vera-deck;
+        }
+        {
+          allowedIPs = [ "${vera-phone}/32" ];
+          publicKey = keys.vera-phone;
+        }
+      ];
+    };
 
-  networking.hosts = {
+    lilynet = with ips.lilynet; {
+      ips = [ "${briah}/24" ];
+      listenPort = 1195;
+      privateKeyFile = private-key;
+      peers = [
+        {
+          allowedIPs = [ "${miracle-crusher}/32" ];
+          publicKey = keys.miracle-crusher;
+        }
+        {
+          allowedIPs = [ "${neith-deck}/32" ];
+          publicKey = keys.neith-deck;
+        }
+        {
+          allowedIPs = [ "${vera-deck}/32" ];
+          publicKey = keys.vera-deck;
+        }
+      ];
+    };
 
+    slugcatnet = with ips.slugcatnet; {
+      ips = [ "${briah}/24" ];
+      listenPorts = 1196;
+      privateKeyFile = private-key;
+      peers = [
+        {
+          allowedIPs = [ "${remie}/32" ];
+          publicKey = keys.remie;
+        }
+        {
+          allowedIPs = [ "${miracle-crusher}/32" ];
+          publicKey = keys.miracle-crusher;
+        }
+        {
+          allowedIPs = [ "${luna}/32" ];
+          publicKey = keys.luna;
+        }
+        {
+          allowedIPs = [ "${neith-deck}/32" ];
+          publicKey = keys.neith-deck;
+        }
+      ];
+    };
+  };
+
+  networking.firewall.trustedInterfaces = [ "gradientnet" "lilynet" "slugcatnet" ];
+  systemd.network.wait-online.ignoredInterfaces = [ "gradientnet" "lilynet" "slugcatnet" ];
+
+  networking.hosts = with ips; {
+    "${gradientnet.miracle-crusher}" = [ "vera" ];
+    "${gradientnet.vera-deck}" = [ "deck" ];
+    "${lilynet.neith-deck}" = [ "lily" "neith" ];
   };
 }
