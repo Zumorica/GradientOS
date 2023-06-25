e16b50db4e9e51e44a1ceae06e1696506c275443
Vera Aguilera Puerto <gradientvera@outlook.com>
2023-06-25 13:24:27 +0200

    Add networkd module.

---
+++ modules/networkd.nix
@@ -0,0 +1,21 @@
+{ ... }:
+{
+
+  systemd.network.enable = true;
+
+  services.dnsmasq = {
+    enable = true;
+    resolveLocalQueries = true;
+    settings = {
+      server = [
+        "1.1.1.1"
+        "1.0.0.1"
+        "8.8.8.8"
+        "8.8.4.4"
+      ];
+    };
+  };
+
+  networking.resolvconf.enable = true;
+
+}
