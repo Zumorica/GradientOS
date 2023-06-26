5072f1d1d706db3cc05920d4b82838bfcdc6844f
Vera Aguilera Puerto <gradientvera@outlook.com>
2023-06-26 20:40:10 +0200

    fix trilium, again

--- hosts/briah/trilium-memory-repository.nix
+++ hosts/briah/trilium-memory-repository.nix
@@ -3,7 +3,7 @@ let
   ports = import misc/service-ports.nix;
 in {
 
-  config.virtualisation.oci-containers.containers.memory-repository = {
+  virtualisation.oci-containers.containers.memory-repository = {
     image = "zadam/trilium:0.60-latest";
     ports = [ "127.0.0.1:${toString ports.trilium}:8080" ];
     volumes = [ "/data/trilium:/home/node/trilium-data" ];
