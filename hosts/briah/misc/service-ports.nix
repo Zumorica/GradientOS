394d363912484a166943964911556aed9ffdcf79
Vera Aguilera Puerto <gradientvera@outlook.com>
2023-06-26 20:39:05 +0200

    add ports, trilium fix

--- hosts/briah/misc/service-ports.nix
+++ hosts/briah/misc/service-ports.nix
@@ -1,5 +1,7 @@
 {
 
-  trilium = "8081";
+  gradient-moe = 80;
+  gradient-moe-secure = 443;
+  trilium = 8081;
 
 }
--- hosts/briah/trilium-memory-repository.nix
+++ hosts/briah/trilium-memory-repository.nix
@@ -5,8 +5,10 @@ in {
 
   config.virtualisation.oci-containers.containers.memory-repository = {
     image = "zadam/trilium:0.60-latest";
-    ports = [ "127.0.0.1:${ports.trilium}:8080" ];
+    ports = [ "127.0.0.1:${toString ports.trilium}:8080" ];
     volumes = [ "/data/trilium:/home/node/trilium-data" ];
   };
+
+  networking.firewall.allowedTCPPorts = [ ports.trilium ];
 
 }
