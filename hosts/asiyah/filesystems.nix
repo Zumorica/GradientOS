fb1f14ae0ae9c5cf8f4cf8f5d478690f32372fd4
Vera Aguilera Puerto <gradientvera@outlook.com>
2023-06-27 16:45:24 +0200

    Remove data disk from asiyah Azure is too darn expensive...

--- hosts/asiyah/filesystems.nix
+++ hosts/asiyah/filesystems.nix
@@ -6,11 +6,6 @@
       fsType = "ext4";
     };
 
-  fileSystems."/mnt/resource" =
-    { device = "/dev/disk/by-uuid/5f9b9e9d-ad9d-4567-8e79-8db8241fc877";
-      fsType = "ext4";
-    };
-
   swapDevices = [ ];
 
 }
