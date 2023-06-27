e051572963609346893ccadc8c2f7a56f4939f1d
Vera Aguilera Puerto <gradientvera@outlook.com>
2023-06-27 12:24:23 +0200

    ipv6 default forward

--- hosts/briah/wireguard.nix
+++ hosts/briah/wireguard.nix
@@ -29,6 +29,8 @@ in {
   boot.kernel.sysctl = {
     "net.ipv4.conf.all.forwarding" = lib.mkOverride 98 true;
     "net.ipv4.conf.default.forwarding" = lib.mkOverride 98 true;
+    "net.ipv6.conf.all.forwarding" = lib.mkOverride 98 true;
+    "net.ipv6.conf.default.forwarding" = lib.mkOverride 98 true;
   };
 
   networking.wireguard.interfaces = {
