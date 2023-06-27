6543aca47b5e8b9e55156fd8bc3bfef003afe21b
Vera Aguilera Puerto <gradientvera@outlook.com>
2023-06-27 11:51:30 +0200

    briah nginx config

--- hosts/briah/nginx.nix
+++ hosts/briah/nginx.nix
@@ -1,6 +1,49 @@
-{ ... }:
-{
+{ self, ... }:
+let
+  ports = import misc/service-ports.nix;
+  ips = import ../../misc/wireguard-addresses.nix;
+in {
 
-  
+  services.nginx = {
+    enable = true;
+
+    recommendedGzipSettings = true;
+    recommendedOptimisation = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+
+    virtualHosts = {
+      "gradient.moe" = {
+        root = self.inputs.gradient-moe;
+        enableACME = true;
+        forceSSL = true;
+      };
+      "gradient" = {
+        locations."memory_repository" = {
+          proxyPass = "http://127.0.0.1:${toString ports.trilium}";
+          extraConfig = ''
+            proxy_http_version 1.1;
+            proxy_set_header Upgrade $http_upgrade;
+            proxy_set_header Connection 'upgrade';
+            proxy_set_header Host $host;
+            proxy_cache_bypass $http_upgrade;
+            allow ${ips.gradientnet}/24;
+            deny all;
+          '';
+        };
+      };
+    };
+  };
+
+  security.acme = {
+    acceptTerms = true;
+    defaults = {
+      email = "gradientvera+acme@outlook.com";
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = with ports; [
+    gradient-moe gradient-moe-secure
+  ];
 
 }
