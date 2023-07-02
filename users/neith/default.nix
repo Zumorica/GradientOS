b4e3f3e3005c5d16771488d67248ed9c062a0455
Vera Aguilera Puerto <gradientvera@outlook.com>
2023-07-02 18:21:09 +0200

    Enable user lingering

--- users/neith/default.nix
+++ users/neith/default.nix
@@ -10,4 +10,9 @@
     hashedPassword = "$6$7mwTIbQIbSE9s6h5$J1Z5xG3V5kY65pgSQKulKg5UpVUnKuHnZoXmZ98IMCRNXhLHWgEAbizz8g4d1IJvDMp/pLBl4EKK.0fzcyb6N0";
   };
 
+  # Temporary workaround to enable user lingering.
+  systemd.tmpfiles.rules = [
+    "f /var/lib/systemd/linger/vera"
+  ];
+
 }
--- users/vera/default.nix
+++ users/vera/default.nix
@@ -10,4 +10,9 @@
     hashedPassword = "$6$mTrvQELm1M1xnRO3$C8.NuZcgEKqW.QHFjABHk4Wkufa4FT0VpAzzgbuF1nwpx719/91uOpnq5JgY1C9LOi55d49VSp7H.KJ/iy74r.";
   };
 
+  # Temporary workaround to enable user lingering.
+  systemd.tmpfiles.rules = [
+    "f /var/lib/systemd/linger/vera"
+  ];
+
 }
