1d190695eeeb5393b8e14e9a9650f703e9c12028
Vera Aguilera Puerto <gradientvera@outlook.com>
2023-07-03 11:11:04 +0200

    Tweaks to bluetooth config

--- hardware/bluetooth.nix
+++ hardware/bluetooth.nix
@@ -5,6 +5,19 @@
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
+    settings = {
+      General = {
+        Class = "0x000100";
+        ControllerMode = "dual";
+        FastConnectable = true;
+        JustWorksRepairing = "always";
+        Privacy = "device";
+      };
+      Policy = {
+        ReconnectIntervals = "1,1,2,3,5,8,13,21,34,55";
+        AutoEnable = true;
+      };
+    };
   };
 
 }
