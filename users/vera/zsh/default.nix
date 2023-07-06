8e0f55acd6eb35ea72b3b2f53f3408197a79955b
Vera Aguilera Puerto <gradientvera@outlook.com>
2023-07-06 19:18:28 +0200

    Use new zsh option

--- users/vera/zsh/default.nix
+++ users/vera/zsh/default.nix
@@ -6,8 +6,8 @@
     enable = true;
     enableCompletion = true;
     enableAutosuggestions = true;
-    enableSyntaxHighlighting = true;
     enableVteIntegration = true;
+    syntaxHighlighting.enable = true;
     historySubstringSearch.enable = true;
     oh-my-zsh = {
       enable = true;
