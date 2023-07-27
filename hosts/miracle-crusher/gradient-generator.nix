89142405b8c344b532098e4b680b577d09851b03
Vera Aguilera Puerto <gradientvera@outlook.com>
2023-07-27 13:17:45 +0200

    Update gradient-generator, gradient-moe - Adds new mastodon bot configuration.

--- hosts/miracle-crusher/gradient-generator.nix
+++ hosts/miracle-crusher/gradient-generator.nix
@@ -7,9 +7,11 @@ in {
     enable = true;
     enableDiscordUpload = false;
     enableMastodonUpload = false;
+    enableMastodonBotUpload = true;
     firefoxProfile = "/home/vera/.mozilla/firefox/be78j9ok.default-release";
     gmicUpdateFile = "/home/vera/.config/gmic/update322.gmic";
     mastodonUrl = "https://tech.lgbt";
+    mastodonBotUrl = "https://botsin.space";
     user = "vera";
   };
 
