458b9a6b3cb0e26dc3ec7cd1382efff49a93bc6f
Vera Aguilera Puerto <gradientvera@outlook.com>
2023-08-21 22:12:33 +0200

    Add discord verification to domain

--- hosts/asiyah/nginx.nix
+++ hosts/asiyah/nginx.nix
@@ -116,6 +116,45 @@ in {
         ];
       };
 
+      "neith.constellation.moe" = {
+        enableACME = true;
+        addSSL = true;
+
+        locations."/" = {
+          return = "301 https://constellation.moe$request_uri";
+        };
+        
+        locations."/.well-known/discord" = {
+          return = "200 dh=c6efaef2bdecf33aa37bf661e003f57442183211";
+        };
+      };
+
+      "remie.constellation.moe" = {
+        enableACME = true;
+        addSSL = true;
+        
+        locations."/" = {
+          return = "301 https://constellation.moe$request_uri";
+        };
+
+        locations."/.well-known/discord" = {
+          return = "200 dh=1bcf1ec16a3b9abcf5df802a38bf0de78075303a";
+        };
+      };
+
+      "vera.constellation.moe" = {
+        enableACME = true;
+        addSSL = true;
+        
+        locations."/" = {
+          return = "301 https://constellation.moe$request_uri";
+        };
+
+        locations."/.well-known/discord" = {
+          return = "200 dh=5d358d1a3d245e6375922193e53fdd0ab83b619b";
+        };
+      };
+
     };
   };
 
