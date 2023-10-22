24cb6c211e605a19680a8da7863d5817ccf2f472
Vera Aguilera Puerto <gradientvera@outlook.com>
2023-10-22 19:59:25 +0200

    Add oversteer to logitech driving wheels mixin.

--- mixins/hardware/logitech-driving-wheels.nix
+++ mixins/hardware/logitech-driving-wheels.nix
@@ -1,7 +1,12 @@
-{ ... }:
+{ pkgs, ... }:
 {
 
   # Better support
   hardware.new-lg4ff.enable = true;
+
+  # Steering wheel manager
+  environment.systemPackages = [
+    pkgs.oversteer
+  ];
 
 }
