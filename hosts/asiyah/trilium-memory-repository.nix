464ab5e4f1f939c116a1ca1d88d073b2417f2746
Vera Aguilera Puerto <gradientvera@outlook.com>
2023-11-04 16:48:07 +0100

    Update Trilium to 0.61

--- hosts/asiyah/trilium-memory-repository.nix
+++ hosts/asiyah/trilium-memory-repository.nix
@@ -4,7 +4,7 @@ let
 in {
 
   virtualisation.oci-containers.containers.memory-repository = {
-    image = "zadam/trilium:0.60-latest";
+    image = "zadam/trilium:0.61-latest";
     ports = [ "127.0.0.1:${toString ports.trilium}:8080" ];
     volumes = [ "/data/trilium:/home/node/trilium-data" ];
     environment = { TZ = config.time.timeZone; };
