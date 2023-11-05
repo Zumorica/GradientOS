abc7c62f26da7e4e851f96cf1adfca1e75f5cdeb
Vera Aguilera Puerto <gradientvera@outlook.com>
2023-11-05 12:03:05 +0100

    Hopefully fix booting on miracle-crusher. - Use linux 6.5, 6.6 seems broken? - Make specialisation with zen kernel, for further testing

--- hosts/miracle-crusher/kernel.nix
+++ hosts/miracle-crusher/kernel.nix
@@ -1,8 +1,13 @@
-{ config, pkgs, nixpkgs-unstable, ... }:
+{ pkgs, lib, ... }:
 
 {
 
-  boot.kernelPackages = pkgs.linuxPackages_zen;
+  specialisation.zen-kernel.configuration = {
+    boot.kernelPackages = lib.mkForce pkgs.linuxPackages_zen;
+  };
+
+  boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_5;
+  
   boot.kernelParams = [  ];
 
 }
--- mixins/hardware/amdgpu.nix
+++ mixins/hardware/amdgpu.nix
@@ -4,8 +4,6 @@
 
   environment.variables.AMD_VULKAN_ICD = "RADV";
 
-  services.xserver.videoDrivers = [ "amdgpu" ];
-
   hardware.opengl.extraPackages = with pkgs; [
     vaapiVdpau
     libvdpau-va-gl
