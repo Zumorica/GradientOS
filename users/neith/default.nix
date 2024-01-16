b2ab9bc01429e8572efdea2c90b24e06e9f7c022
Vera Aguilera Puerto <gradientvera@outlook.com>
2024-01-16 18:36:26 +0100

    Use official NixOS user lingering method instead of old workaround

--- users/neith/default.nix
+++ users/neith/default.nix
@@ -4,15 +4,11 @@
 
   users.users.neith = {
     isNormalUser = true;
+    linger = true;
     description = "Neith";
     shell = pkgs.zsh;
     extraGroups = [ "networkmanager" "wheel" "audio" "video" "scanner" "lp" ];
     hashedPassword = "$6$7mwTIbQIbSE9s6h5$J1Z5xG3V5kY65pgSQKulKg5UpVUnKuHnZoXmZ98IMCRNXhLHWgEAbizz8g4d1IJvDMp/pLBl4EKK.0fzcyb6N0";
   };
-
-  # Temporary workaround to enable user lingering.
-  systemd.tmpfiles.rules = [
-    "f /var/lib/systemd/linger/neith"
-  ];
 
 }
--- users/vera/default.nix
+++ users/vera/default.nix
@@ -4,15 +4,11 @@
 
   users.users.vera = {
     isNormalUser = true;
+    linger = true;
     description = "Vera";
     shell = pkgs.zsh;
     extraGroups = [ "networkmanager" "wheel" "audio" "video" "scanner" "lp" "libvirtd" "openrazer" ];
     hashedPassword = "$6$mTrvQELm1M1xnRO3$C8.NuZcgEKqW.QHFjABHk4Wkufa4FT0VpAzzgbuF1nwpx719/91uOpnq5JgY1C9LOi55d49VSp7H.KJ/iy74r.";
   };
-
-  # Temporary workaround to enable user lingering.
-  systemd.tmpfiles.rules = [
-    "f /var/lib/systemd/linger/vera"
-  ];
 
 }
