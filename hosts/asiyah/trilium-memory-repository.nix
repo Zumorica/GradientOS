797e3590c029e50c7deadec75c1a5b2dc2a1e613
Vera Aguilera Puerto <gradientvera@outlook.com>
2024-01-28 21:46:36 +0100

    Override trilium container image Add pyodide to node modules, for use inside trilium scripts.

--- hosts/asiyah/trilium-memory-repository.nix
+++ hosts/asiyah/trilium-memory-repository.nix
@@ -1,10 +1,32 @@
-{ config, ... }:
+{ config, pkgs, lib, ... }:
 let
   ports = import misc/service-ports.nix;
+  name = "trilium-memory-repository";
+  tag = "0.62.4"; # Corresponds to original image tag.
 in {
 
   virtualisation.oci-containers.containers.memory-repository = {
-    image = "zadam/trilium:0.62-latest";
+    image = "${name}:${tag}";
+    imageFile = with pkgs.dockerTools;
+      buildImage {
+        inherit name tag;
+        config.Cmd = [ "./start-memory-repository.sh" ];
+        config.WorkingDir = "/usr/src/app";
+        fromImage = pullImage {
+          imageName = "zadam/trilium";
+          imageDigest = "sha256:cfc1e54c0cdc733e904eb2e3e49a248bc4b4c8a4ae393a0155d69acaa88d920f";
+          sha256 = "sha256-66tmPanpAZpddnm6tKDyl1dLBVHRU4m+Ghta+/7vNJw=";
+        };
+        runAsRoot = ''
+          #!${pkgs.runtimeShell}
+          cd /usr/src/app
+          cp ${(pkgs.writeScript "start-memory-repository" ''
+            #!/bin/sh
+            npm install pyodide
+            ./start-docker.sh
+          '')} ./start-memory-repository.sh
+        '';
+      };
     ports = [ "127.0.0.1:${toString ports.trilium}:8080" ];
     volumes = [ "/data/trilium:/home/node/trilium-data" ];
     environment = { TZ = config.time.timeZone; };
