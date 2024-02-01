26c724e53d61bba60fcc91358940e67b33484096
Vera Aguilera Puerto <gradientvera@outlook.com>
2024-02-01 21:41:43 +0100

    Do not set SDL_VIDEODRIVER to wayland Usually breaks things, best to set it on a case-by-case basis...

--- mixins/graphical/wayland/environment-variables.nix
+++ mixins/graphical/wayland/environment-variables.nix
@@ -4,7 +4,7 @@
   # Wayland support for most applications.
   environment.sessionVariables = {
     NIXOS_OZONE_WL = "1";
-    SDL_VIDEODRIVER = "wayland";
+    # SDL_VIDEODRIVER = "wayland"; # Breaks more things than not...
     GDK_BACKEND = "wayland";
     QT_QPA_PLATFORM = "wayland";
     XDG_SESSION_TYPE = "wayland";
