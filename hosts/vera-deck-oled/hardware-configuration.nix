b6b4747cef8fe58312dbeaded03a86a811c0df47
Vera Aguilera Puerto <gradientvera@outlook.com>
2024-02-27 22:58:34 +0100

    Enable unl0kr on vera-deck-oled - Disable plymouth in a hacky way, temporarily.

--- hosts/vera-deck-oled/hardware-configuration.nix
+++ hosts/vera-deck-oled/hardware-configuration.nix
@@ -6,11 +6,14 @@
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usbhid" "sdhci_pci" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usbhid" "sdhci_pci" "hid-multitouch" "evdev" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
+  boot.plymouth.enable = lib.mkForce false;
+  boot.initrd.unl0kr.enable = true;
+  
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
