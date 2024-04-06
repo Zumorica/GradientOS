9b09ad2e227aadc602102bf96f6b157c57cabd16
Vera Aguilera Puerto <gradientvera@outlook.com>
2024-04-06 15:52:55 +0200

    Add randomized delay to hokma backups. Prevent concurrent backups from duplicating files (although they would be cleaned up next run, but.)

--- mixins/restic/repository-hokma.nix
+++ mixins/restic/repository-hokma.nix
@@ -15,6 +15,8 @@ in
     
     timerConfig = {
       OnCalendar = "Mon *-*-* 10:00:00";
+      # Prevent concurrent backups, as it can lead to duplicate files
+      RandomizedDelaySec = "5h";
     };
 
     # Set these on your host!
