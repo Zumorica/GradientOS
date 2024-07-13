bc1595a89b3d61788edcd60d2d557cb53198d43b
Vera Aguilera Puerto <gradientvera@outlook.com>
2024-07-13 12:02:22 +0200

    Add lact to amdgpu

--- mixins/hardware/amdgpu.nix
+++ mixins/hardware/amdgpu.nix
@@ -4,7 +4,12 @@
 
   environment.variables.AMD_VULKAN_ICD = "RADV";
 
+  systemd.packages = with pkgs; [
+    lact
+  ];
+
   hardware.graphics.extraPackages = with pkgs; [
+    lact
     vaapiVdpau
     libvdpau-va-gl
     rocm-opencl-icd
